27e11842e51b393ee1ef87918710533c1ec36e95
aslbekadizov2001 <aslbekadizov2001@gmail>
2022-11-25 20:08:57 +0500

    first

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ let menu = document.querySelector('.burger_icon')
 let burger_menu = document.querySelector('.burger_menu')
 let word = document.querySelector('.header_slider h4')
 let img1 = document.querySelector('.img1')
-
+let menu_1 = document.querySelectorAll('#menu_1')
+let menu_title = document.querySelectorAll('.menu_title')
 
 
 // HEADER_SLIDER_START
-var i = 1;
+let i = 1;
 images[i].style.opacity = "1"
 setInterval(function(){
     images[i].style.opacity = "0"
@@ -40,7 +41,6 @@ burger_menu.addEventListener('click', function(){
 $(function () {
     $(window).scroll(function () {
         let $scroll = $(this).scrollTop()
-        console.log($scroll)
         if($scroll > 300){
             $('.scroll').fadeIn()
         }
@@ -55,3 +55,69 @@ $(function () {
     })
 });
 // SCROLL_FINISH
+
+
+// MENU_START
+// console.log(menu_1.length)
+// function event_menu(){
+//     p = 0; p <= menu_title.length; p++
+//     return (event_menu)
+// }
+
+for(j = 1; j < menu_1.length; j++){
+    menu_1[2].addEventListener('click', function (){
+        menu_title[2].style.display = 'block'
+    })
+    menu_1[3].addEventListener('click', function (){
+        menu_title[3].style.display = 'block'
+    })
+    menu_1[4].addEventListener('click', function (){
+        menu_title[4].style.display = 'block'
+    })
+    menu_1[5].addEventListener('click', function (){
+        menu_title[5].style.display = 'block'
+    })
+    menu_1[6].addEventListener('click', function (){
+        menu_title[6].style.display = 'block'
+    })
+    menu_1[7].addEventListener('click', function (){
+        menu_title[7].style.display = 'block'
+    })
+    menu_1[8].addEventListener('click', function (){
+        menu_title[8].style.display = 'block'
+    })
+}
+// MENU_FINISH
+
+// SLIDER_SECTION_START
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        margin: 10,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        // autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: true
+            },
+            400:{
+                items: 3,
+                nav: true
+            },
+            800:{
+                items: 4,
+                nav: true
+            },
+            1200:{
+                items: 6,
+                nav: true
+            }
+
+        }
+    })
+});
+// SLIDER_SECTION_FINISH

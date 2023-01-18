253669fee98db41f156d076e9703e97eaa6f56f1
aslbekadizov2001 <aslbekadizov2001@gmail.com>
2023-01-18 12:28:19 +0500

    1index finish

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ let menu = document.querySelector('.burger_icon')
 let burger_menu = document.querySelector('.burger_menu')
 let word = document.querySelector('.header_slider h4')
 let img1 = document.querySelector('.img1')
-let menu_1 = document.querySelectorAll('.menu')
+let menu_1 = document.querySelectorAll('.menu p')
 let menu_title = document.querySelectorAll('.menu_title')
-let dish_menu = document.querySelectorAll('.dish_p')
+let dish_menu = document.querySelectorAll('.dish_p p')
 let dish_title = document.querySelectorAll('.dish_title')
 
 
@@ -32,9 +32,11 @@ window.addEventListener('load', function(e){
 // BURGER_MENU_START
 menu.addEventListener('click', function(){
     burger_menu.style.right = '0px'
+    document.querySelector('body').style = `overflow:hidden;`
 })
 burger_menu.addEventListener('click', function(){
     burger_menu.style.right = '-325px'
+    document.querySelector('body').style = `overflow:none;`
 })
 // BURGER_MENU_FINISH
 
@@ -60,55 +62,36 @@ $(function () {
 
 
 // MENU_START
-for(let u = 0; u < 8; u++){
-    menu_1[u].addEventListener('click', function(){
-        menu_1[0].style.border = 'none'
-        menu_1[1].style.border = 'none'
-        menu_1[2].style.border = 'none'
-        menu_1[3].style.border = 'none'
-        menu_1[4].style.border = 'none'
-        menu_1[5].style.border = 'none'
-        menu_1[6].style.border = 'none'
-        menu_1[7].style.border = 'none'
-        menu_1[u].style.border = '2px solid #fff'
-        menu_1[u].style.borderRight = 'none'
+menu_1.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        menu_1.forEach(item => {
+            item.classList.remove('active')
+        })
         
-        // Border 
-        console.log(menu_title[u])
-        menu_title[0].style.display = 'none'
-        menu_title[1].style.display = 'none'
-        menu_title[2].style.display = 'none'
-        menu_title[3].style.display = 'none'
-        menu_title[4].style.display = 'none'
-        menu_title[5].style.display = 'none'
-        menu_title[6].style.display = 'none'
-        menu_title[7].style.display = 'none'
-        menu_title[u].style.display = 'block'
-        
+        item.classList.add('active')
+        menu_title.forEach(item => {
+            item.style = `display: none;`
+        });
+        menu_title[index].style = `display: block;`
     })
-}
+});
 // MENU_FINISH
 
 
 // MENU2_START
-for(let h = 0; h < 5; h++){
-    dish_menu[h].addEventListener('click', function () {
-        dish_menu[0].style.border = 'none'
-        dish_menu[1].style.border = 'none'
-        dish_menu[2].style.border = 'none'
-        dish_menu[3].style.border = 'none'
-        dish_menu[4].style.border = 'none'
-        dish_menu[h].style.border = '1px solid #69e3f3'
-        dish_menu[h].style.borderRight = 'none'
-        // BORDER
-        dish_title[0].style.display = 'none'
-        dish_title[1].style.display = 'none'
-        dish_title[2].style.display = 'none'
-        dish_title[3].style.display = 'none'
-        dish_title[4].style.display = 'none'
-        dish_title[h].style.display = 'block'
+dish_menu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        dish_menu.forEach(item => {
+            item.classList.remove('active2')
+        })
+        
+        item.classList.add('active2')
+        dish_title.forEach(item => {
+            item.style = `display: none;`
+        });
+        dish_title[index].style = `display: block;`
     })
-}
+});
 // MENU2_FINISH
 
 // SLIDER_SECTION_START
